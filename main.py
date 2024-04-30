4ffb6dd910f926d2d084b0326025d7701ced4e9f
Paul Koski <pwkoski@gmail.com>
2024-04-30 14:24:10 -0400

    Add five recipes, update ingredient schema, create ingredient list function

--- main.py
+++ main.py
@@ -4,14 +4,35 @@ import json
 import os
 
 
-display(os.getcwd())
-display(os.listdir())
+#display(os.getcwd(), append=True)
+#display(os.listdir(), append=True)
+
+def generate_ingredient_list(recipe_list):
+    ingredient_array = []
+    for recipe in recipe_list["recipes"]:
+        for i in range(0,len(recipe["ingredients_and_amounts_in_milliliters"]), 2):
+            if recipe["ingredients_and_amounts_in_milliliters"][i] not in ingredient_array:
+                ingredient_array.append(recipe["ingredients_and_amounts_in_milliliters"][i])
+    return ingredient_array
 
 
-df = pd.read_json('recipes.json')
 
-display(df, append=False)
-#display(df2, append=True)
+
+with open('recipes.json') as json_file:
+    recipe_list = json.load(json_file)
+
+    # Print the type of data variable
+    #display(type(data), append=True)
+
+    # Print the data of dictionary
+    display(generate_ingredient_list(recipe_list), append=True)
+
+
+#recipe_dict = js.load('recipes.json')
+#df = pd.read_json('recipes.json')
+
+#display(df, append=True)
+#display(recipe_dict, append=True)
 
 
 
