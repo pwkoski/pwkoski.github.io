979ef3f3a9422e2a29135fede799f81bd55ce2c0
Paul Koski <pwkoski@gmail.com>
2024-05-08 13:26:31 -0400

    Rewrite the dataframe generator as a recursive function, not sure it's quite there yet.

--- main.py
+++ main.py
@@ -4,55 +4,42 @@ import json
 
 
 with open('ingredients.json') as json_file:
-    #Create dict from json
+
+
+    def df_recursive_create(array_of_dicts):
+
+        df = pd.DataFrame()
+
+        def recursion(dictionary, index):
+            #for key, value in dictionary.items():
+            #display(print(dictionary.keys()))
+            for key in dictionary:
+                #display(print(key))
+                if type(dictionary[key]) is dict:
+                    return recursion(dictionary[key], index)
+                else:
+                    df.at[index, key] = dictionary[key]
+
+        for i in range(0, len(array_of_dicts)):
+            for dictionary in array_of_dicts[i]:
+                recursion(array_of_dicts[i][dictionary], i)
+            #array_of_dicts[i] is not a dict but a dict of dicts
+
+
+        return df
+
+#Create dict from json
     recipe_list = json.load(json_file)
 
-    #Build dataframe manually from dict
+#Build dataframe automatically from dict
 
-    df2 = pd.DataFrame(columns=['Name',
-                                "Growable?",
-                                "Price Per Month ($)",
-                                "Fresh?",
-                                "Composite?",
-                                "Ingredient Density (g/mL)",
-                                "Grams Yield per Plant (g/plant)",
-                                "Planting Density (plants/sq. m)",
-                                "Amount per Container (g/container)",
-                                "Storage Type",
-                                "Container",
-                                "Container Area Density (containers/sq. m)",
-                                "Container Volume Density (containers/cu. m)",
-                                "Protein %",
-                                "Carb %",
-                                "Fat %",
-                                "Caloric Density (kcal/g)"
-                                ])
+    df1 = df_recursive_create(recipe_list["ingredients"])
+
+#Display
+
+    display(df1, append=True)
 
 
-    r = recipe_list["ingredients"]
-    for i in range(len(r)):
-       recipe = r[i]
-       df2.loc[i] = [recipe["info"]["name"],
-                     recipe["info"]["growable"],
-                     recipe["info"]["price_per_month"],
-                     recipe["info"]["fresh"],
-                     recipe["info"]["composite"],
-                     recipe["info"]["ingredient_grams_per_milliliter"],
-                     recipe["growth"]["grams_yield_per_plant"],
-                     recipe["growth"]["plants_per_square_meter"],
-                     recipe["storage"]["grams_per_container"],
-                     recipe["storage"]["storage_type"],
-                     recipe["storage"]["storage"],
-                     recipe["storage"]["containers_per_square_meter"],
-                     recipe["storage"]["containers_per_cubic_meter"],
-                     recipe["nutrition"]["protein_percentage"],
-                     recipe["nutrition"]["carb_percentage"],
-                     recipe["nutrition"]["fat_percentage"],
-                     recipe["nutrition"]["calories_per_gram"]
-                     ]
-
-    #Display
-    display(df2, append=True)
 
 
 
--- old_functions.py
+++ old_functions.py
@@ -1,3 +1,79 @@
+#Recursive function that I built the recursive df create function
+
+def recursive_items(dictionary):
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            yield from recursive_items(value)
+        else:
+            yield (key, value)
+
+for key, value in recursive_items(dictionary):
+    display(key, value)
+
+
+
+
+#not really a function but this section of code will build the
+#ingredient dataframe manually.  This may still be the primary way
+#to build if the recursive version doesn't work.
+#I: Depends on how you look at it, but either a json file, a dict, or, an array of dicts.
+#O: A dataframe with all the ingredient data
+#C: relies on knowing the column names first.
+
+with open('ingredients.json') as json_file:
+    #Create dict from json
+    recipe_list = json.load(json_file)
+
+    #Build dataframe manually from dict
+
+    df2 = pd.DataFrame(columns=['Name',
+                                "Growable?",
+                                "Price Per Month ($)",
+                                "Fresh?",
+                                "Composite?",
+                                "Ingredient Density (g/mL)",
+                                "Grams Yield per Plant (g/plant)",
+                                "Planting Density (plants/sq. m)",
+                                "Amount per Container (g/container)",
+                                "Storage Type",
+                                "Container",
+                                "Container Area Density (containers/sq. m)",
+                                "Container Volume Density (containers/cu. m)",
+                                "Protein %",
+                                "Carb %",
+                                "Fat %",
+                                "Caloric Density (kcal/g)"
+                                ])
+
+
+    r = recipe_list["ingredients"]
+    for i in range(len(r)):
+       recipe = r[i]
+       df2.loc[i] = [recipe["info"]["name"],
+                     recipe["info"]["growable"],
+                     recipe["info"]["price_per_month"],
+                     recipe["info"]["fresh"],
+                     recipe["info"]["composite"],
+                     recipe["info"]["ingredient_grams_per_milliliter"],
+                     recipe["growth"]["grams_yield_per_plant"],
+                     recipe["growth"]["plants_per_square_meter"],
+                     recipe["storage"]["grams_per_container"],
+                     recipe["storage"]["storage_type"],
+                     recipe["storage"]["storage"],
+                     recipe["storage"]["containers_per_square_meter"],
+                     recipe["storage"]["containers_per_cubic_meter"],
+                     recipe["nutrition"]["protein_percentage"],
+                     recipe["nutrition"]["carb_percentage"],
+                     recipe["nutrition"]["fat_percentage"],
+                     recipe["nutrition"]["calories_per_gram"]
+                     ]
+
+
+
+
+
+
+
 #generates a list of ingredients based on the recipe.json recipe list.
 #I: recipe list, a dict with a single key (recipes) whose value is an array of recipes
 #O: an array of strings, each string a unique ingredient in the recipes
